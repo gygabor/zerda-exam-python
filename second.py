3fdcc4cd41bbcbb1f4eb8e20fadf8a8ca4395b7b
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-05 09:52:12 +0100

    python exam, exercise 2 is done

--- second.py
+++ second.py
@@ -4,3 +4,17 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+import os.path
+
+def write_text(file_name, get_string):
+
+    if os.path.isfile(file_name) and type(get_string) is str:
+        text_file = open(file_name, 'w')
+        for i in range(10):
+            text_file.write(get_string)
+        text_file.close()
+        return True
+    else:
+        return False
+
+print (write_text('text.txt', 'apple'))
