a792f91e76956dbf2244678ed88d11dd04c632ca
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-05 09:28:33 +0100

    python exam, exercise 1 is done

--- first.py
+++ first.py
@@ -2,3 +2,12 @@
 # and returns a new list with every second element from the original list.
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
+
+def find_second(number_list):
+        if type(number_list) is not list:
+            raise TypeError('The parameter is not a list')
+        else:
+            return number_list[1::2]
+
+
+print(find_second([1, 2, 3, 4, 5, 6, 7]))
