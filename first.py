58d4c8a662bb2df95325e937f1a382e9df295c00
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-05 09:29:39 +0100

    python exam, exercise 1: modified

--- first.py
+++ first.py
@@ -4,10 +4,10 @@
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
 
 def find_second(number_list):
-        if type(number_list) is not list:
-            raise TypeError('The parameter is not a list')
-        else:
-            return number_list[1::2]
+    if type(number_list) is not list:
+        raise TypeError('The parameter is not a list')
+    else:
+        return number_list[1::2]
 
 
 print(find_second([1, 2, 3, 4, 5, 6, 7]))
